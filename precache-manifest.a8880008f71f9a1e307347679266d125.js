d7ededf0b85adcfd1a7e6df1308cb2692172b6f0
mansMS <47422885+mansMS@users.noreply.github.com>
2020-01-06 12:05:19 +0300

    Updates

---
+++ precache-manifest.a8880008f71f9a1e307347679266d125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797bb649aec42f33a178a1cc53db9eda",
+    "url": "/amazing-hiring-2/index.html"
+  },
+  {
+    "revision": "2fe9de5dc8c259361c56",
+    "url": "/amazing-hiring-2/static/css/main.003f924f.chunk.css"
+  },
+  {
+    "revision": "ee044e0957afb1da209d",
+    "url": "/amazing-hiring-2/static/js/2.d11594eb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/amazing-hiring-2/static/js/2.d11594eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fe9de5dc8c259361c56",
+    "url": "/amazing-hiring-2/static/js/main.4391c7f0.chunk.js"
+  },
+  {
+    "revision": "03db9f9fb3c95e44f4c7",
+    "url": "/amazing-hiring-2/static/js/runtime-main.fdfc62a3.js"
+  }
+]);
