de1736e6a601f1892f0a62a0041771f75d37607a
mansMS <47422885+mansMS@users.noreply.github.com>
2020-01-07 15:38:33 +0300

    Minor fixes

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,11 +5,22 @@ const Table = ({ tableData, changeSortingParams, sortField, sortDirection }) => 
 
   if (!tableData.length) return <p>Нет данных</p>
 
+  const tableHeaders = Object.keys(tableData[0]);
+
+  const sortedTableContent = tableData.sort((a, b) => {
+    if (sortDirection === 'asc') {
+      if (a[sortField] < b[sortField]) return -1
+    } else {
+      if (a[sortField] > b[sortField]) return -1
+    }
+    return 0
+  });
+
   return (
     <table>
       <thead>
         <tr onClick={changeSortingParams}>
-          {Object.keys(tableData[0]).map((columnName, index) =>
+          {tableHeaders.map((columnName, index) =>
             <th key={index} value={columnName}>
               {columnName}
               {sortField === columnName && <span className={sortDirection}></span>}
@@ -19,14 +30,7 @@ const Table = ({ tableData, changeSortingParams, sortField, sortDirection }) => 
       </thead>
 
       <tbody>
-        {tableData.sort((a, b) => {
-          if (sortDirection === 'asc') {
-            if (a[sortField] < b[sortField]) return -1
-          } else {
-            if (a[sortField] > b[sortField]) return -1
-          }
-          return 0
-        }).map((row, index) =>
+        {sortedTableContent.map((row, index) =>
           <tr key={index}>
             {Object.keys(row).map((cell, index) => <td key={index}>{row[cell]}</td>)}
           </tr>
